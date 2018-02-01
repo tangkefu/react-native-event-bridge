94be0e9a9da448922cac93a977bda1ded6e93d7f
Michael Schneider <mischneider1@gmail.com>
2018-02-01 11:54:55 -0800

    Log if an event was sent but is not supported

--- android/src/main/java/net/mischneider/MSREventBridgeModule.java
+++ android/src/main/java/net/mischneider/MSREventBridgeModule.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.AssertionException;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
@@ -33,6 +34,8 @@ import java.util.Map;
  * Module that handles receiving and sending events from and to React Native
  */
 public class MSREventBridgeModule extends ReactContextBaseJavaModule implements MSREventBridgeEventEmitter {
+
+  public static final String TAG = "MSREventBridgeModule";
 
   // Static Identifier for events that are sent to React Native. These needs to be in sync with iOS!
   private static final String EventBridgeModuleName = "MSREventBridge";
@@ -123,11 +126,16 @@ public class MSREventBridgeModule extends ReactContextBaseJavaModule implements 
 
         List<String> supportedEvents = receiver.supportedEvents();
         if (!supportedEvents.contains(name)) {
+          if (BuildConfig.DEBUG) {
+            Log.d(TAG, String.format("%s is not a supported event type for %s. Supported events are: `%s`", name, receiver.getClass().getName(), supportedEvents.toString()));
+          }
           return;
         }
 
         boolean eventHandled = receiver.onEvent(name, info);
-        // TODO: Throw an assertion if eventHandled == false
+        if (BuildConfig.DEBUG && !eventHandled) {
+          throw new AssertionException(String.format("Event supported but not handled: <name: %s, info:%s>", name, info.toString()));
+        }
       }
     });
   }
@@ -153,9 +161,11 @@ public class MSREventBridgeModule extends ReactContextBaseJavaModule implements 
           return;
         }
 
-
         List<String> supportedEvents = receiver.supportedEvents();
         if (!supportedEvents.contains(name)) {
+          if (BuildConfig.DEBUG) {
+            Log.d(TAG, String.format("%s is not a supported event type for %s. Supported events are: `%s`", name, receiver.getClass().getName(), supportedEvents.toString()));
+          }
           return;
         }
 
@@ -170,7 +180,10 @@ public class MSREventBridgeModule extends ReactContextBaseJavaModule implements 
             callback.invoke(data, null);
           }
         });
-        // TODO: Throw an assertion if eventHandled == false
+
+        if (BuildConfig.DEBUG && !eventHandled) {
+          throw new AssertionException(String.format("Event supported but not handled: <name: %s, info:%s>", name, info.toString()));
+        }
       }
     });
   }
