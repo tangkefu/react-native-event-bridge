c24b3831b5f4836fa817aabf1e4d9ad45e060bf3
tommy <tang.kefu@hotmail.com>
2020-01-03 16:22:23 +0800

    bug

--- android/src/main/java/net/mischneider/MSREventBridgeModule.java
+++ android/src/main/java/net/mischneider/MSREventBridgeModule.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.view.View;
 import android.view.ViewGroup;
 
