7334886d6c0149ae1375005446a6bc9df5843575
tommy <tang.kefu@hotmail.com>
2020-01-03 16:17:40 +0800

    android x

--- android/src/main/java/net/mischneider/MSREventBridgeAwareReactRootView.java
+++ android/src/main/java/net/mischneider/MSREventBridgeAwareReactRootView.java
@@ -1,9 +1,9 @@
 package net.mischneider;
 
 import android.content.Context;
-import android.support.annotation.AttrRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.AttrRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.facebook.react.ReactRootView;
--- android/src/main/java/net/mischneider/MSREventBridgeEventEmitter.java
+++ android/src/main/java/net/mischneider/MSREventBridgeEventEmitter.java
@@ -1,7 +1,7 @@
 package net.mischneider;
 
 import android.app.Activity;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.WritableMap;
 
